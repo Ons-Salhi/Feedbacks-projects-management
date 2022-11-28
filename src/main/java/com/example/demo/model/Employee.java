2c082d9a93b2920e4a5c8ade69005b5f568b3491
Ons-Salhi <salhi.ons@icloud.com>
2022-11-28 22:14:12 +0100

    first commit

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+@Data
+@Entity
+@Table(name="employees")
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "department")
+    private String department;
+
+
+
+}
